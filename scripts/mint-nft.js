63dcf4be1bc4bbbe2273e1b14f38038b037e7bef
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-13 11:49:11 +0800

    Added code for printing the ABI in the console

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -5,3 +5,7 @@ const web3 = createAlchemyWeb3(API_URL);
 
 // Grabbing contract's ABI
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+// Printing the ABI in the console
+console.log(JSON.stringify(contract.abi));
+
+// Command for printing the ABI in the console: 'node scripts/mint-nft.js'
