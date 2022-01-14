e64ab6f806a70b46ca264a35e0e829c20b694873
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-14 12:00:22 +0800

    Created instance of contract

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -5,7 +5,13 @@ const web3 = createAlchemyWeb3(API_URL);
 
 // Grabbing contract's ABI
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
 // Printing the ABI in the console
 console.log(JSON.stringify(contract.abi));
 
-// Command for printing the ABI in the console: 'node scripts/mint-nft.js'
+/* 
+Command for printing the ABI in the console: 'node scripts/mint-nft.js'
+*/
+
+const contractAddress = "0xe8B00e6d6DE63a9c4d3d3b881d2199505eb03C5C";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
