9b8621fb452c1c4d1a70db0d4d72e88d82c14bbc
Dante O. Cuales <danteocualesjr@gmail.com>
2022-06-13 07:09:12 +0800

    Refactored code

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
-
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
 const web3 = createAlchemyWeb3(API_URL);
