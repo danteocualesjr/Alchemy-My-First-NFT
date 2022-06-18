6937e9dd9fa6590bcbf8673afadd2593789a14f7
Dante O. Cuales <danteocualesjr@gmail.com>
2022-06-18 11:52:53 +0800

    Refactored code

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -12,7 +12,8 @@ const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 const contractAddress = "0xe8B00e6d6DE63a9c4d3d3b881d2199505eb03C5C";
 const nftContract= new web3.eth.Contract(contract.abi, contractAddress);
 
-async function mintNFT(tokenURI) {    
+async function mintNFT(tokenURI) {   
+     
     const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); // Get latest nonce
 
     // The transaction    
